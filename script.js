d15e34cd501b346ab9d89d5798615ade7d9c8a8e
rbabar1 <118569152+rbabar1@users.noreply.github.com>
2023-04-26 23:57:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// ************w************
+
+//     Do not alter this comment block. 
+//     Only fill out the information below.
+    
+//     Project 4 - Web Design
+//     Name: Hassan Babar
+//     Date: Apr 26, 2023
+//     Description: Web Design
+//
+//**************/
+
+$('#menu-button').click(function()
+{
+    $(' .nav-links ul').addClass('active');
+})
